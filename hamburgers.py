54d0accdf40772db5fd0ee789d34f8763690cab3
bbundy12 <105405942+bbundy12@users.noreply.github.com>
2023-03-01 19:54:39 -0700

    comm

--- hamburgers.py
+++ hamburgers.py
@@ -49,7 +49,7 @@ while iNumCustomers > 0:
 
     lstHamburgerQueue.append(Customer())
     iNumCustomers -= 1
-    if Customer.customer_name in dictCustomer:
+    if Customer.customer_name in dictCustomers:
         dictCustomers[Customer.customer_name] += Customer.order 
 
 #print(f"{name} {burgers}")
