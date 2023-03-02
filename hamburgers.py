248ada5a80b96a1b73786986fb7f1c7eee97fdfd
bbundy12 <105405942+bbundy12@users.noreply.github.com>
2023-03-01 19:51:55 -0700

    c

--- hamburgers.py
+++ hamburgers.py
@@ -31,8 +31,16 @@ class Customer(Person):
 
 lstHamburgerQueue = []
 
-dictCustomersOrders = {
-    
+dictCustomers = {
+    "Jefe" : 0,
+    "el Guapo" : 0,
+    "Lucky Day" : 0,
+    "Ned Nederlander" : 0,
+    "Dusty Bottoms" : 0,
+    "Harry Flugleman" : 0,
+    "Carmen" : 0,
+    "Invisible Swordsman" : 0,
+    "Singing Bush" : 0
 }
 
 iNumCustomers = 100
