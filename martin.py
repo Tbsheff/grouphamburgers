5e74129165932df1d1d359f17fc9e09875beaba8
pochovillar <m.villar.barrios@gmail.com>
2023-03-01 18:26:02 -0700

    ..

--- martin.py
+++ martin.py
@@ -1,4 +1,4 @@
 class Customer(Person): 
-    def _innit__ (): 
-     super().__innit__()
-     self.order = Order()
+    def _innit__ (self): 
+      super().__innit__()
+      self.order = Order()
