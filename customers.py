cd05812b2f8db63dc4552b17ae824141de81299a
Tyler Sheffield <tylersheffield427@gmail.com>
2023-03-01 18:27:19 -0700

    commit

--- customers.py
+++ customers.py
@@ -1,1 +1,19 @@
 # This will create a customer class
+
+
+class Customer ():
+    
+    pass
+
+
+lstHamburgerQueue = []
+
+iNumCustomers = 100
+
+while iNumCustomers > 0 :
+    
+    lstHamburgerQueue.append(Customer())
+    iNumCustomers -=1
+
+
+
