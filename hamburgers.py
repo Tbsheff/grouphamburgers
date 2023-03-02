a569bbd22e74d9b138ee8b960dde2671261ee66f
Tyler Sheffield <tylersheffield427@gmail.com>
2023-03-01 18:35:17 -0700

    commit

--- hamburgers.py
+++ hamburgers.py
@@ -8,3 +8,26 @@ class Order():
     
     def randomBurger():
         return rd(1,20)
+
+<<<<<<< HEAD
+
+class Customer ():
+    
+    pass
+
+
+lstHamburgerQueue = []
+
+iNumCustomers = 100
+
+while iNumCustomers > 0 :
+    
+    lstHamburgerQueue.append(Customer())
+    iNumCustomers -=1
+
+=======
+class Customer(Person): 
+    def _innit__ (self): 
+      super().__innit__()
+      self.order = Order()
+>>>>>>> 6e4a82e7d474c87501f55b61026387d450bff15f
