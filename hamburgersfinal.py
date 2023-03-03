32cceef6ecb67595906d2c6132cc9aee0650d38c
savannahhogan <63937202+savannahhogan@users.noreply.github.com>
2023-03-02 18:15:40 -0700

    commit changes

--- hamburgersfinal.py
+++ hamburgersfinal.py
@@ -1,28 +1,32 @@
-# Group 7 Hamburger Project
+#Group 7: Tyler Sheffield, Brendan Bundy, Camille (Hannah) Cameron, Hannah Johnson, Martin Villar, Savannah Hogan
+#Hamburger Group Project 
+#This projects creates and stores a dictionary of customers from a list, 
+#randomizes orders for each customer, and tracks the total number of hamburgers ordered 
 
+#import random
 import random
 
-
+#create order class 
 class Order():
     def __init__(self):
         self.burger_count = self.randomBurger()
 
+#create method to generate random burger count 
     def randomBurger(self):
         return random.randint(1, 20)
 
-
+#create person class 
 class Person():
-    # constructor
     def __init__(self):
         self.customer_name = self.randomName()
-    # method
-
+   
+    #method to generate a random customer from a list 
     def randomName(self):
         lstCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms",
                         "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
         return random.choice(lstCustomers)
 
-
+#create customer class that inherits from the person class 
 class Customer(Person):
     def __init__(self):
         super().__init__()
@@ -30,6 +34,7 @@ class Customer(Person):
 
 
 lstHamburgerQueue = []
+
 
 dictCustomers = {
     "Jefe": 0,
@@ -43,7 +48,9 @@ dictCustomers = {
     "Singing Bush": 0
 }
 
+
 iNumCustomers = 100
+
 
 for iCount in range(0, iNumCustomers):
 
