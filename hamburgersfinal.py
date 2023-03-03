6940873b2fd47375c98097795ff4ac995215479f
Tyler Sheffield <tylersheffield427@gmail.com>
2023-03-02 18:46:41 -0700

    commit

--- hamburgersfinal.py
+++ hamburgersfinal.py
@@ -1,42 +1,49 @@
-#Group 7: Tyler Sheffield, Brendan Bundy, Camille (Hannah) Cameron, Hannah Johnson, Martin Villar, Savannah Hogan
-#Hamburger Group Project 
-#This projects creates and stores a dictionary of customers from a list, 
-#randomizes orders for each customer, and tracks the total number of hamburgers ordered 
+# Group 7: Tyler Sheffield, Brendan Bundy, Camille (Hannah) Cameron, Hannah Johnson, Martin Villar, Savannah Hogan
+# Hamburger Group Project
+# This projects creates and stores a dictionary of customers from a list,
+# randomizes orders for each customer, and tracks the total number of hamburgers ordered
 
-#import random
+# import random
 import random
 
-#create order class 
+# create order class
+
+
 class Order():
-    #create a constructor that defines an instance variable called burger_count
+    # create a constructor that defines an instance variable called burger_count
     def __init__(self):
         self.burger_count = self.randomBurger()
 
-    #create a method called randomBurgers that returns a number between 1 and 20
+    # create a method called randomBurgers that returns a number between 1 and 20
     def randomBurger(self):
         return random.randint(1, 20)
 
-#create person class 
+# create person class
+
+
 class Person():
-    #create a constructor that defines an instance variable called customer_name
+    # create a constructor that defines an instance variable called customer_name
     def __init__(self):
         self.customer_name = self.randomName()
-   
-    #create a method called randomName() that contains a list of 9 names
+
+    # create a method called randomName() that contains a list of 9 names
     def randomName(self):
         lstCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms",
                         "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
         return random.choice(lstCustomers)
 
-#create customer class that inherits from the person class 
+# create customer class that inherits from the person class
+
+
 class Customer(Person):
-    #create a constructor that calls the parent constructor
+    # create a constructor that calls the parent constructor
     def __init__(self):
         super().__init__()
-        #create an instance variable called order in the constructor that is assigned an order object
+        # create an instance variable called order in the constructor that is assigned an order object
         self.order = Order()
 
-#create queue
+
+# create queue
 lstHamburgerQueue = []
 
 
@@ -63,11 +70,11 @@ for iCount in range(0, iNumCustomers):
         dictCustomers[lstHamburgerQueue[0].customer_name] += lstHamburgerQueue[0].order.burger_count
     lstHamburgerQueue.pop(0)
 
-#sort customers by the most number of burgers ordered to the least number ordered
+# sort customers by the most number of burgers ordered to the least number ordered
 listSortedCustomers = sorted(
     dictCustomers.items(), key=lambda x: x[1], reverse=True)
 
-#print out each customer and their total burgers
+# print out each customer and their total burgers
 print("\n")
 for iCount in range(0, len(listSortedCustomers)):
     print(
