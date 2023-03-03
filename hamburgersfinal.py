43b38c8b15ab331cc5f8f9ce2c5f62f35d12c505
Hannahkj12 <hannahkj2001@gmail.com>
2023-03-02 18:19:54 -0700

    commit

--- hamburgersfinal.py
+++ hamburgersfinal.py
@@ -28,9 +28,10 @@ class Customer(Person):
         super().__init__()
         self.order = Order()
 
-
+#create queue
 lstHamburgerQueue = []
 
+#create dictionary with keys of type string and variables of type int
 dictCustomers = {
     "Jefe": 0,
     "El Guapo": 0,
@@ -43,8 +44,10 @@ dictCustomers = {
     "Singing Bush": 0
 }
 
+#put 100 customers into the queue
 iNumCustomers = 100
 
+#add customer object to queue and keep track of the number of orders they make
 for iCount in range(0, iNumCustomers):
 
     lstHamburgerQueue.append(Customer())
@@ -52,10 +55,11 @@ for iCount in range(0, iNumCustomers):
         dictCustomers[lstHamburgerQueue[0].customer_name] += lstHamburgerQueue[0].order.burger_count
     lstHamburgerQueue.pop(0)
 
-
+#sort customers by the most number of burgers ordered to the least number ordered
 listSortedCustomers = sorted(
     dictCustomers.items(), key=lambda x: x[1], reverse=True)
 
+#print out each customer and their total burgers ordered 
 for iCount in range(0, len(listSortedCustomers)):
     print(
         f'{listSortedCustomers[iCount][0].ljust(19)}\t{listSortedCustomers[iCount][1]}')
